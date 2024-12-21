97334237c68a217edc448b17ce28e715abc06ef5
Nicolas Pellerin <nicolas-clark@device-34.home>
2024-12-21 16:26:09 +0100

    sending 200 OK response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[allow(unused_imports)]
+use std::io::prelude::*;
 use std::net::TcpListener;
 
 fn main() {
@@ -9,10 +10,17 @@ fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
+    let ok_response = b"HTTP/1.1 200 OK\r\n\r\n";
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let res = stream.write(ok_response);
+                match res {
+                    Ok(_) => println!("Successfully sent 200 OK response"),
+                    Err(e) => println!("Error sending 200 OK response: {}", e),
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
