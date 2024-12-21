e8993bbcb6d367bac3a765c4dab7122a61c1ea94
Nicolas Pellerin <nicolas-clark@device-34.home>
2024-12-21 18:05:24 +0100

    OK response on the right request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 #[allow(unused_imports)]
 use std::io::prelude::*;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+fn is_html_request_complete(buf: &Vec<u8>) -> bool {
+    let end_pattern = "\r\n\r\n"; // double CRFL
+    buf.windows(end_pattern.len()).any(|w| w == b"\r\n\r\n")
+}
+
+fn read_request(stream: &mut TcpStream) -> Option<Vec<u8>> {
+    let mut buf = Vec::new();
+    let mut tmp_buf = [0; 1024];
+    loop {
+        match stream.read(&mut tmp_buf) {
+            Ok(n) => {
+                buf.extend_from_slice(&tmp_buf[..n]);
+                if is_html_request_complete(&buf) {
+                    return Some(buf);
+                }
+            }
+            Err(e) => {
+                println!("Problem reading the request: {}", e);
+                return None;
+            }
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,14 +40,22 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let res = stream.write(ok_response);
-                match res {
-                    Ok(_) => println!("Successfully sent 200 OK response"),
-                    Err(e) => println!("Error sending 200 OK response: {}", e),
+
+                let buf: Vec<u8> = read_request(&mut stream).unwrap();
+                let full_request = String::from_utf8(buf).expect("Our bytes should be valid utf8");
+                println!("Request: {}", full_request);
+                let request_line = full_request.split("\n").collect::<Vec<&str>>()[0];
+                dbg!(request_line);
+                if *"GET /" == request_line[..5] {
+                    let res = stream.write(ok_response);
+                    match res {
+                        Ok(_) => println!("Successfully sent 200 OK response"),
+                        Err(e) => println!("Error sending 200 OK response: {}", e),
+                    }
                 }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error accepting the connection: {}", e);
             }
         }
     }
