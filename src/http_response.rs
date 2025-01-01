59d429453f1873cc74c9ca373a973a460e98b87d
Nicolas Pellerin <nicolas-clark@Nicolass-MacBook-Pro.local>
2025-01-01 23:51:57 +0100

    fix hex-string formatting (remove inner "")

--- src/http_response.rs
+++ src/http_response.rs
@@ -232,7 +232,7 @@ impl Display for HttpResponse {
                         .join(" ");
                     write!(
                         f,
-                        "{} {}\r\nContent-Type: {}\r\n{}\r\nContent-Length: {}\r\n\r\n{:02X?}",
+                        "{} {}\r\nContent-Type: {}\r\n{}\r\nContent-Length: {}\r\n\r\n{}",
                         self.protocol_version,
                         self.status_code,
                         self.content_type,
