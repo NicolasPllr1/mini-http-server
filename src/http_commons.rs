4479cc765336f2cbd32f887307e641a5d9ed9213
Nicolas Pellerin <nicolas-clark@Nicolass-MacBook-Pro.local>
2025-01-23 19:20:58 +0100

    better error handling in the request + couple of enhancements

--- src/http_commons.rs
+++ src/http_commons.rs
@@ -4,6 +4,19 @@ pub enum HttpVersion {
     Http2,
     // Http3,
 }
+
+impl std::str::FromStr for HttpVersion {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "HTTP/1.1" => Ok(Self::Http11),
+            "HTTP/2" => Ok(Self::Http2),
+            _ => Err("Unsupported HTTP version".into()),
+        }
+    }
+}
+
 impl std::fmt::Display for HttpVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
--- src/http_request.rs
+++ src/http_request.rs
@@ -20,40 +20,41 @@ pub enum HttpMethod {
     POST,
 }
 
+impl std::str::FromStr for HttpMethod {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::GET),
+            "POST" => Ok(Self::POST),
+            _ => Err("Unsupported HTTP method".into()),
+        }
+    }
+}
+
 impl HttpRequest {
     pub fn build_from_stream(stream: &mut TcpStream) -> Result<HttpRequest, Box<dyn Error>> {
         let mut reader = BufReader::new(&mut *stream);
 
         // Read the *request-line*
         let mut request_line = String::new();
-        match reader.read_line(&mut request_line) {
-            Ok(_) => println!("Sucess reading the *request-line*: {}", request_line),
-            Err(e) => return Err(format!("Error reading the *request-line*: {}", e).into()),
-        };
+        reader.read_line(&mut request_line)?;
+        println!("Sucess reading the *request-line*: {}", request_line);
 
         // Parse the *request-line*
-        let (http_method, request_target, protocol_version) = match request_line
+        let [http_method, request_target, protocol_version]: [&str; 3] = request_line
             .split_whitespace()
-            .collect::<Vec<&str>>()[..]
-        {
-            [m, p, v] => (m, p.to_string(), v),
-            _ => return Err(
+            .collect::<Vec<_>>()
+            .try_into()
+            .map_err(|_| {
                 "Invalid HTTP request-line. Expected: <method> <request-target> <protocol-version>"
-                    .into(),
-            ),
-        };
+            })?;
 
-        let http_method = match http_method {
-            "GET" => HttpMethod::GET,
-            "POST" => HttpMethod::POST,
-            _ => return Err("Unsupported HTTP method".into()),
-        };
+        let request_target = request_target.to_string();
 
-        let protocol_version = match protocol_version {
-            "HTTP/1.1" => HttpVersion::Http11,
-            "HTTP/2" => HttpVersion::Http2,
-            _ => return Err(format!("Not HTTP/1.1 nor HTTP/2. Got: {}", protocol_version).into()),
-        };
+        let http_method = http_method.parse::<HttpMethod>()?; // turbofish yeah + shadowing
+
+        let protocol_version = protocol_version.parse::<HttpVersion>()?;
 
         // Read eventual *headers*
         let mut headers: HashMap<String, String> = HashMap::new();
@@ -70,18 +71,20 @@ impl HttpRequest {
         }
 
         // Read the body if any
-        let body: Option<String> = match headers.get("Content-Length") {
-            Some(n_bytes_str) => {
-                let n_bytes = n_bytes_str
-                    .parse::<usize>()
-                    .expect("Error parsing the content length");
-                let mut body_buf = vec![0; n_bytes];
-                reader
-                    .read_exact(&mut body_buf)
-                    .expect("Could not read body into buffer");
-                Some(String::from_utf8(body_buf).expect("Invalid utf-8 for body"))
-            }
-            None => None,
+        let body = if let Some(n_bytes_str) = headers.get("Content-Length") {
+            let n_bytes = n_bytes_str
+                .parse::<usize>()
+                .map_err(|e| format!("Invalid content-length (for body): {}", e))?;
+
+            let mut body_buf = vec![0; n_bytes];
+            reader.read_exact(&mut body_buf)?;
+
+            Some(
+                String::from_utf8(body_buf)
+                    .map_err(|e| format!("Invalid utf-8 for body content: {}", e))?,
+            )
+        } else {
+            None
         };
 
         Ok(HttpRequest {
